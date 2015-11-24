60b882055589c6b6dd3e7acb890de1de6c640213
viher3 <viher_3@hotmail.com>
2015-11-24 17:00:46 +0100

    title post seo

--- blog/wp-content/themes/twentytwelve/content.php
+++ blog/wp-content/themes/twentytwelve/content.php
@@ -25,7 +25,7 @@
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php else : ?>
 			<h1 class="entry-title">
-				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
 			<?php endif; // is_single() ?>
 			<?php if ( comments_open() ) : ?>
