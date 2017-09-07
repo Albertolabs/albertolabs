e6b0610421409cdfe724495363b8708de6e04a71
viher3 <viher_3@hotmail.com>
2017-09-07 15:21:16 +0200

    projects home php

--- web/pages/home/projects.php
+++ web/pages/home/projects.php
@@ -7,153 +7,31 @@
 
 	<!-- portfolio items -->		
 	<div class="content-wrap" style="padding:0;">
-
-	<?php /*
-		<!-- Portfolio Filter -->
-		============================================= 
-		<ul id="portfolio-filter" class="clearfix">
-
-			<li class="activeFilter"><a href="#" data-filter="*">All projects</a></li>
-			<li><a href="#" data-filter=".pf-icons">Software</a></li>
-			<li><a href="#" data-filter=".pf-illustrations">Websites</a></li>
-			<li><a href="#" data-filter=".pf-uielements">Others</a></li>
-
-		</ul><!-- #portfolio-filter end -->
-		<!--
-		<div id="portfolio-shuffle">
-			<i class="icon-random"></i>
-		</div>
-
-		*/ ?>
 		
 		<div class="clear"></div>
 
 		<!-- Portfolio Items -->
 		<div id="portfolio" class="clearfix">
 
-			<article class="portfolio-item pf-media pf-icons">
+			<?php
+				require 'data/projectsData.php';
+				foreach($projectsData as $key => $project){
+			?>
+			<article class="portfolio-item pf-media <?= $project['filters'] ?>">
 				<div class="portfolio-image">
-					<img src="assets/images/projects/sombeo-thumb.jpg" title="Sombeo" alt="Sombeo" />
-
+					<img src="assets/images/projects/<?= $project['img_thumb'] ?>" title="<?= $project['title'] ?>" alt="<?= $project['title'] ?>" />
 					<div class="portfolio-overlay">
-						<a href="./projects/sombeo.php" title="Ver más sobre Sombeo" class="right-icon" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
+						<a href="#" title="Ver más sobre <?= $project['title'] ?>" class="right-icon"><i class="icon-line-plus"></i></a>
 					</div>
 				</div>
 				<div class="portfolio-desc">
 					<h3>
-						<a href="./projects/sombeo.php" title="Ver más sobre Sombeo" class="right-icon" data-lightbox="iframe">Sombeo</a>
+						<a href="<?= $project['route'] ?>" title="Ver más sobre <?= $project['title'] ?>" class="right-icon"><?= $project['title'] ?></a>
 					</h3>
-					<span>Website</span>
+					<span><?= $project['type'] ?></span>
 				</div>
 			</article>
-
-			<article class="portfolio-item pf-media pf-icons">
-				<div class="portfolio-image">
-
-					<img title="Perretes" src="assets/images/projects/perretes-thumb.jpg" title="Perretes" alt="Perretes" />
-
-					<div class="portfolio-overlay">
-						<a href="./projects/perretes.php" title="Ver más sobre Perretes" class="right-icon" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
-					</div>
-				</div>
-				<div class="portfolio-desc">
-					<h3>
-						<a href="./projects/perretes.php" title="Ver más sobre Perretes" class="right-icon" data-lightbox="iframe">Perretes</a>
-					</h3>
-					<span>Website</span>
-				</div>
-			</article>
-
-			<article class="portfolio-item pf-media pf-icons">
-				<div class="portfolio-image">
-					<img src="assets/images/projects/womp-thumb.jpg" alt="Womp" title="Womp" />
-					<div class="portfolio-overlay">
-						<a href="./projects/womp.php" title="Womp" title="Ver más sobre Womp" class="right-icon" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
-					</div>
-				</div>
-				<div class="portfolio-desc">
-					<h3>
-						<a href="./projects/womp.php" title="Womp" title="Ver más sobre Womp" class="right-icon" data-lightbox="iframe">Womp</a>
-					</h3>
-					<span>Website</span>
-				</div>
-			</article>
-
-			<article class="portfolio-item pf-media pf-icons">
-				<div class="portfolio-image">
-					<img src="assets/images/projects/receptormixto-thumb.jpg" alt="Receptormixto" title="Receptormixto">
-					<div class="portfolio-overlay">
-						<a href="./projects/receptormixto.php" title="Receptormixto" class="right-icon" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
-					</div>
-				</div>
-				<div class="portfolio-desc">
-					<h3>
-						<a href="./projects/receptormixto.php" title="Receptormixto" class="right-icon" data-lightbox="iframe">Receptormixto</a>
-					</h3>
-					<span>Software, Web application</span>
-				</div>
-			</article>
-
-			<article class="portfolio-item pf-media pf-icons">
-				<div class="portfolio-image">
-					<img src="assets/images/projects/team-data-repository-thumb.jpg" alt="Team Data Repository Software" title="Team Data Repository Software" />
-					<div class="portfolio-overlay">
-						<a href="./projects/team-data-repository.php" title="Team data repository Software" class="right-icon" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
-					</div>
-				</div>
-				<div class="portfolio-desc">
-					<h3>
-						<a href="./projects/team-data-repository.php" title="Team data repository Software" class="right-icon" data-lightbox="iframe">Team data repository</a>
-					</h3>
-					<span>Software, Web application</a>
-					</span>
-				</div>
-			</article>
-
-			<article class="portfolio-item pf-media pf-icons">
-				<div class="portfolio-image">
-					<img src="assets/images/projects/team-data-repository-website-thumb.jpg" alt="Team Data Repository Website" title="Team Data Repository Website" />
-					<div class="portfolio-overlay">
-						<a href="./projects/team-data-repository-website.php" title="Team data repository" class="right-icon" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
-					</div>
-				</div>
-				<div class="portfolio-desc">
-					<h3>
-						<a href="./projects/team-data-repository-website.php" title="Team data repository" class="right-icon" data-lightbox="iframe">Team data repository</a>
-					</h3>
-					<span>Website</span>
-				</div>
-			</article>
-
-			<article class="portfolio-item pf-media pf-icons">
-				<div class="portfolio-image">
-					<img src="assets/images/projects/blog-reaccionestudio-thumb.jpg" alt="Blog Reaccionestudio" title="Blog Reaccionestudio" />
-					<div class="portfolio-overlay">
-						<a href="./projects/blog-reaccionestudio.php" title="Blog Reaccionestudio" class="right-icon" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
-					</div>
-				</div>
-				<div class="portfolio-desc">
-					<h3>
-						<a href="./projects/blog-reaccionestudio.php" title="Blog Reaccionestudio" class="right-icon" data-lightbox="iframe">Blog Reaccion Estudio</a>
-					</h3>
-					<span>Website</span>
-				</div>
-			</article>
-
-			<article class="portfolio-item pf-media pf-icons">
-				<div class="portfolio-image">
-					<img src="assets/images/projects/reaccionestudio-thumb.jpg" alt="Reaccionestudio" title="Reaccionestudio" />
-					<div class="portfolio-overlay">
-						<a href="./projects/reaccionestudio.php" class="right-icon" title="Reaccionestudio" data-lightbox="iframe"><i class="icon-line-plus"></i></a>
-					</div>
-				</div>
-				<div class="portfolio-desc">
-					<h3>
-						<a href="./projects/reaccionestudio.php" class="right-icon" title="Reaccionestudio" data-lightbox="iframe">Reaccion Estudio</a>
-					</h3>
-					<span>Website</span>
-				</div>
-			</article>
+			<?php } ?>
 
 		</div><!-- #portfolio end -->
 
