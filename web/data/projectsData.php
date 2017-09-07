658459abd858d867ca211aa941b4d3d5ddbb8012
viher3 <viher_3@hotmail.com>
2017-09-07 16:38:01 +0200

    project details on projectsData

--- web/data/projectsData.php
+++ web/data/projectsData.php
@@ -8,7 +8,10 @@
 			'title' => 'Lantigua Casa Rural',
 			'type' => 'Website',
 			'filters' => 'pf-websites',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Lantigua Casa Rural</p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'WordPress', 'PHP 7', 'MySQL', 'Apache', 'RuralGest'),
+			'demo' => 'http://www.lantiguacasarural.com/'
 		),
 		'sombeo' => array(
 			'img_thumb' => 'sombeo-thumb.jpg',
@@ -16,7 +19,10 @@
 			'title' => 'Sombeo',
 			'type' => 'Website',
 			'filters' => 'pf-websites',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Sombeo is a website based on Wordpress where we post funny things every day. This website has been adapted to our needs: we have modified some Wordpress functionalities and plugins, the main template, etc.</p><p>We have developed various automation process whose are executed through crontab:</p><p>First of them, gets content from various websites. After this, content must be approved by administrators for being published in the main page. This lets us to publish quickly the best and funniest content from the Internet in our website.</p><p>Another one, it is a process used to auto-posting pre-defined content in our Facebook page using Facebook SDK. It isn\'t only about sharing posts, this process uploads the images and videos content to the Facebook page.</p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'WordPress', 'PHP 5', 'MySQL', 'Apache' ),
+			'demo' => 'http://www.sombeo.com/'
 		),
 		'perretes' => array(
 			'img_thumb' => 'perretes-thumb.jpg',
@@ -24,7 +30,10 @@
 			'title' => 'Perretes',
 			'type' => 'Website',
 			'filters' => 'pf-websites',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Perretes is a website where users can search and publish dog adoption advertisements in Spain completely free.</p><p>Dog adoption advertisements are classified by province, breed, size, weight, genre and age for a better search experience.</p><p>We have developed an automation process which is executed through crontab and crawls various websites getting dog adoption advertisements.</p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'Bootstrap 3', 'Responsive design', 'CodeIgniter 2', 'PHP 5', 'MySQL', 'Apache' ),
+			'demo' => 'http://www.perretes.net/'
 		),
 		'womp' => array(
 			'img_thumb' => 'womp-thumb.jpg',
@@ -32,7 +41,10 @@
 			'title' => 'Womp',
 			'type' => 'Website',
 			'filters' => 'pf-websites',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Womp was a young web development company which offer all kind of custom web solutions for their customers.</p><p>We developed the last website version for Womp, creating a custom CMS and a responsive design with Bootstrap 3.</p><p>Actually, Womp isn\'t using this website design, but you can see a theme preview in the left image or clicking on the \'<i>View template</i>\' button. </p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'Bootstrap 3', 'Responsive design', 'PHP 5', 'MySQL' ),
+			'demo' => 'http://www.albertolabs.com/assets/images/projects/womp-full.jpg'
 		),
 		'receptormixto' => array(
 			'img_thumb' => 'receptormixto-thumb.jpg',
@@ -40,15 +52,10 @@
 			'title' => 'Receptormixto',
 			'type' => 'Software, Web application',
 			'filters' => 'pf-websites pf-software',
-			'route' => '#'
-		),
-		'team_data_repository' => array(
-			'img_thumb' => 'team-data-repository-thumb.jpg',
-			'img_full' => 'team-data-repository-full.jpg',
-			'title' => 'Team Data Repository',
-			'type' => 'Software, Web application',
-			'filters' => 'pf-websites pf-software',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Receptormixto is a web application used by a lottery administration where they can upload the results from different raffles.</p><p>Lottery administration has a touch monitor where is displayed the web application which shows the results to the customers.</p><p>Results are classified by raffles and dates, creating a history with all the data.</p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'Bootstrap 3', 'Responsive design', 'PHP 5', 'MySQL', 'Apache' ),
+			'demo' => 'http://www.receptormixto99705.com/'
 		),
 		'easy_eu_cookie_law' => array(
 			'img_thumb' => 'wp-easy-eu-cookie-law.jpg',
@@ -56,7 +63,21 @@
 			'title' => 'Easy Eu Cookie Law',
 			'type' => 'Software, WordPress Plugin',
 			'filters' => 'pf-websites pf-software',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Cookie law is a WordPress Plugin that helps you to comply with the new European cookie law by the easiest way.</p><p>This plugin has different design customizations and it is Multilanguage</p>',
+			'tech' => array('WordPress', 'PHP 5', 'Javascript', 'jQuery'),
+			'demo' => 'https://es.wordpress.org/plugins/easy-eu-cookie-law/'
+		),
+		'team_data_repository' => array(
+			'img_thumb' => 'team-data-repository-thumb.jpg',
+			'img_full' => 'team-data-repository-full.jpg',
+			'title' => 'Team Data Repository',
+			'type' => 'Software, Web application',
+			'filters' => 'pf-websites pf-software',
+			'route' => '#',
+			'description' => '<p>Team Data Repository is an open source web application which stores and centralizes all necessary information for your team obtaining greater productivity for your employees and your company.</p><p>All the information you need on your screen. Don\'t waste more time researching information between tonnes of paper.</p><p>Team Data Repository prevents this process and helps you keeping all your information ordered.</p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'Bootstrap 2', 'Responsive Design', 'CodeIgniter 2', 'PHP 5', 'MySQL', 'Nginx' ),
+			'demo' => 'http://www.teamdatarepository.com/demostration'
 		),
 		'team_data_repository-website' => array(
 			'img_thumb' => 'team-data-repository-website-thumb.jpg',
@@ -64,7 +85,10 @@
 			'title' => 'Team Data Repository',
 			'type' => 'Website',
 			'filters' => 'pf-websites',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Team Data Repository is an open source web application which stores and centralizes all necessary information for your team obtaining greater productivity for your employees and your company.</p><p>We have development a beautiful website for Team Data Repository.</p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'Responsive Design', 'CodeIgniter 2', 'PHP 5', 'MySQL', 'Nginx' ),
+			'demo' => 'http://www.teamdatarepository.com'
 		),
 		'blog_reaccionestudio' => array(
 			'img_thumb' => 'blog-reaccionestudio-thumb.jpg',
@@ -72,7 +96,10 @@
 			'title' => 'Blog Reacción Estudio',
 			'type' => 'Website',
 			'filters' => 'pf-websites',
-			'route' => '#'
+			'route' => '#',
+			'description' => '<p>Reaccionestudio is a web development and graphic design studio founded by young people from Spain who love web development, graphic design, photography, video production, etc.</p><p>Reaccionestudio blog has been developed with Wordpress. Here they post tutorials, tips, snippets, news and new projects.</p>',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'WordPress', 'PHP 7', 'MySQL', 'Ubuntu', 'Nginx', 'Git' ),
+			'demo' => 'http://blog.reaccionestudio.com/'
 		),
 		'reaccionestudio' => array(
 			'img_thumb' => 'reaccionestudio-thumb.jpg',
@@ -80,7 +107,10 @@
 			'title' => 'Reacción Estudio',
 			'type' => 'Website',
 			'filters' => 'pf-websites',
-			'route' => '#'
+			'route' => '#',
+			'description' => 'Reaccionestudio is a web development and graphic design studio founded by young people from Spain who love web development, graphic design, photography, video production, etc.',
+			'tech' => array( 'HTML 5', 'CSS 3', 'Javascript', 'jQuery', 'PHP 7', 'MySQL', 'Ubuntu', 'Nginx', 'Git' ),
+			'demo' => 'http://www.reaccionestudio.com/'
 		),
 
 	);
