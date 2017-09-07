7dc726188939f852e6d647d6fba58f36d4e9cf53
viher3 <viher_3@hotmail.com>
2017-09-07 15:19:54 +0200

    projectsData

---
+++ web/data/projectsData.php
@@ -0,0 +1,70 @@
+<?php
+
+	$projectsData = array(
+
+		'sombeo' => array(
+			'img_thumb' => 'sombeo-thumb.jpg',
+			'img_full' => 'sombeo-full.jpg',
+			'title' => 'Sombeo',
+			'type' => 'Website',
+			'filters' => 'pf-websites',
+			'route' => '#'
+		),
+		'perretes' => array(
+			'img_thumb' => 'perretes-thumb.jpg',
+			'img_full' => 'perretes-full.jpg',
+			'title' => 'Perretes',
+			'type' => 'Website',
+			'filters' => 'pf-websites',
+			'route' => '#'
+		),
+		'womp' => array(
+			'img_thumb' => 'womp-thumb.jpg',
+			'img_full' => 'womp-full.jpg',
+			'title' => 'Womp',
+			'type' => 'Website',
+			'filters' => 'pf-websites',
+			'route' => '#'
+		),
+		'receptormixto' => array(
+			'img_thumb' => 'receptormixto-thumb.jpg',
+			'img_full' => 'receptormixto-full.jpg',
+			'title' => 'Receptormixto',
+			'type' => 'Software, Web application',
+			'filters' => 'pf-websites pf-software',
+			'route' => '#'
+		),
+		'team_data_repository' => array(
+			'img_thumb' => 'team-data-repository-thumb.jpg',
+			'img_full' => 'team-data-repository-full.jpg',
+			'title' => 'Team Data Repository',
+			'type' => 'Software, Web application',
+			'filters' => 'pf-websites pf-software',
+			'route' => '#'
+		),
+		'team_data_repository-website' => array(
+			'img_thumb' => 'team-data-repository-website-thumb.jpg',
+			'img_full' => 'team-data-repository-website-full.jpg',
+			'title' => 'Team Data Repository',
+			'type' => 'Website',
+			'filters' => 'pf-websites',
+			'route' => '#'
+		),
+		'blog_reaccionestudio' => array(
+			'img_thumb' => 'blog-reaccionestudio-thumb.jpg',
+			'img_full' => 'blog-reaccionestudio-full.jpg',
+			'title' => 'Blog Reacción Estudio',
+			'type' => 'Website',
+			'filters' => 'pf-websites',
+			'route' => '#'
+		),
+		'reaccionestudio' => array(
+			'img_thumb' => 'reaccionestudio-thumb.jpg',
+			'img_full' => 'reaccionestudio-full.jpg',
+			'title' => 'Reacción Estudio',
+			'type' => 'Website',
+			'filters' => 'pf-websites',
+			'route' => '#'
+		),
+
+	);
