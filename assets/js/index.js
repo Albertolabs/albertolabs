2f42c29bdcfe955f94f725c22a86f902db586510
viher3 <viher_3@hotmail.com>
2017-11-08 23:20:49 +0100

    Project.js created and Index.js refactored

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,88 +1,28 @@
 jQuery(window).load(function()
 {
-    // checks url params
-    var currentUrl = window.location;
-    var contactReg = new RegExp("#contact$");
-
-    // goes to contact section
-    if(contactReg.test(currentUrl))
+    if( $.inArray(currRoute, ["home"]) !== -1 )
     {
+        // go to contact section
         var contactPosition = $("#section-contact").offset().top - 200;
         $("html, body").animate({scrollTop:contactPosition + "px"});
-    }
-    // end check url params
 
-    var $container = $('#portfolio');
-
-    $container.isotope({ transitionDuration: '0.65s' });
-
-    function getHashFilter() {
-        var hash = location.hash;
-        // get filter=filterName
-        var matches = location.hash.match( /filter=([^&]+)/i );
-        var hashFilter = matches && matches[1];
-        return hashFilter && decodeURIComponent( hashFilter );
+        // contact form
+        var contact = new Contact();
+            contact.events();
     }
 
-    var isIsotopeInit = false;
-
-    function onHashchange() {
-        var hashFilter = getHashFilter();
-        if ( !hashFilter && isIsotopeInit ) {
-            return;
-        }
-        isIsotopeInit = true;
-        // filter isotope
-        $container.isotope({ filter: hashFilter });
-        // set selected class on button
-        if ( hashFilter ) {
-            $('#portfolio-filter li').removeClass('activeFilter');
-            $('#portfolio-filter li').find('[data-filter="' + hashFilter + '"]').parent('li').addClass('activeFilter');
-        }
-    }
-
-    $(window).on( 'hashchange', onHashchange );
-    // trigger event handler to init Isotope
-    onHashchange();
-
-    $('#portfolio-filter a').click(function(){
-        $('#portfolio-filter li').removeClass('activeFilter');
-        $(this).parent('li').addClass('activeFilter');
-        var selector = $(this).attr('data-filter');
-        $container.isotope({ filter: selector });
-        var filterAttr = $( this ).attr('data-filter');
-        location.hash = 'filter=' + encodeURIComponent( filterAttr );
-        return false;
-    });
-
-    $('#portfolio-shuffle').click(function(){
-        $container.isotope('updateSortData').isotope({
-            sortBy: 'random'
-        });
-    });
-
-    $(window).resize(function() {
-        $container.isotope('layout');
-    });
-
-    $("div.btn-down-scroll a").on("click", function(e)
-    {
-        e.preventDefault();
-
-        var services_pos = $("div#section-services").offset().top;
-        services_pos = services_pos - 200;
-        $('body,html').stop(true).animate({ scrollTop:services_pos + "px" }, 600);
-    });
-
+    // portafolio image click event
     $("div.portfolio-image").on("click", function(e)
     {
         var link = $(this).find("div.portfolio-overlay a");
         var url = link.attr("href");
 
-        if(url.length)
+        if(url.length && url != "#")
         {
             window.location = url;
         }
     });
+
+    
 
 });
--- themes/footer_scripts.php
+++ themes/footer_scripts.php
@@ -2,15 +2,18 @@
 <div id="gotoTop" class="icon-angle-up"></div>
 
 <!-- Footer Scripts -->
+<script type="text/javascript">
+	var currRoute = "<?php echo $router->getCurrentRoute(); ?>";
+</script>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 
 <?php if(app_env == "dev"){ ?>
 <script type="text/javascript" src="assets/js/plugins.js"></script>
 <script type="text/javascript" src="assets/js/functions.js"></script>
 <script type="text/javascript" src="assets/js/index.js"></script>
+<script type="text/javascript" src="assets/js/projects.js"></script>
 <script type="text/javascript" src="assets/js/contact.js"></script>
 <?php }else{ ?>
 <script type="text/javascript" src="assets/js/index.min.js"></script>
 <?php } ?>
-
-<script type="text/javascript">var contact = new Contact();contact.events();</script>
